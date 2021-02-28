5917329e85cacfd1f27554fdef835a7ff9318faf
sforn-null <1641832926@qq.com>
2021-02-28 21:39:22 +0800

    大小端，整型提升，无符号加减等练习

---
+++ 大小端.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//int main()
+//{
+//	//写一个代码告诉我们，当前机器存储采用的是大端还是小端
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
