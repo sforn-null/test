0d1f1b1d577638f5c83a4936d70d7e4025143bc5
sforn-null <1641832926@qq.com>
2021-03-26 23:53:33 +0800

    倒置字符串

---
+++ 倒置字符串.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+
+void reverse(char* left, char* right)
+{
+	while (left < right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+void revserse_string(char* str)
+{
+	char* p = str;
+	while (*str)
+	{
+		char* i = str;
+		char* j = str;
+		while (*i != ' ' && *i != '\0')
+		{
+			i++;
+		}
+		reverse(j, i-1);
+		if (*i == '\0')
+		{
+			reverse(j, p+(strlen(p)-1));
+			str = i;
+		}
+		else
+		{
+			str = i + 1;
+		}
+	}
+	reverse(p, p + strlen(p) - 1);
+}
+int main()
+{
+	char arr[100] = { 0 };
+	//不能用scanf，因为scanf遇到空格就不再输入了
+	char*str = arr;
+	gets_s(arr,100);
+	revserse_string(arr);
+
+	printf("%s", arr);
+	return 0;
+}
