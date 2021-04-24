7a021729cf566e79ca44c8cb4f73a4de027e89c1
sforn-null <1641832926@qq.com>
2021-04-24 22:21:14 +0800

    LeetCode第232题

---
+++ LC232.c
@@ -0,0 +1,202 @@
+﻿//请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+//
+//实现 MyQueue 类：
+//
+//void push(int x) 将元素 x 推到队列的末尾
+//int pop() 从队列的开头移除并返回元素
+//int peek() 返回队列开头的元素
+//boolean empty() 如果队列为空，返回 true ；否则，返回 false
+//
+//
+//说明：
+//
+//你只能使用标准的栈操作 —— 也就是只有 push to top, peek / pop from top, size, 和 is empty 操作是合法的。
+//你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+//
+//
+//进阶：
+//
+//你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
+//
+//
+
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+typedef char STDataType;
+typedef struct Stack
+{
+	STDataType* _a;
+	int _top;//栈顶下标
+	int _capacity;//容量
+}Stack;
+
+void StackInit(Stack* pst);
+//初始化
+
+void StackDestory(Stack* pst);
+//销毁
+
+void StackPush(Stack* pst, STDataType x);
+//入栈
+
+void StackPop(Stack* pst);
+//出栈
+
+int StackSize(Stack* pst);
+//数据个数
+
+int StackEmpty(Stack* pst);
+//返回1是空 返回0是非空
+
+STDataType StackTop(Stack* pst);
+//获得栈顶的数据
+
+
+void StackInit(Stack* pst)
+{
+	/*assert(pst);
+	pst->_a = NULL;
+	pst->_top = 0;
+	pst->_capacity = 0;*/
+	//但是这种方式，第一次放数据不好放不好放
+
+	pst->_a = malloc(sizeof(STDataType) * 4);
+	pst->_top = 0;
+	pst->_capacity = 4;
+}
+
+void StackDestory(Stack* pst)
+{
+	assert(pst);
+	free(pst->_a);
+	pst->_a = NULL;
+	pst->_top = 0;
+	pst->_capacity = 0;
+}
+
+void StackPush(Stack* pst, STDataType x)
+{
+	assert(pst);
+	if (pst->_top == pst->_capacity)
+	{
+		pst->_capacity *= 2;
+		STDataType* tmp = (STDataType*)realloc(pst->_a, sizeof(STDataType) * pst->_capacity);
+		if (tmp == NULL)
+		{
+			printf("内存不足");
+			exit(-1);
+		}
+		else
+		{
+			pst->_a = tmp;
+		}
+	}
+	pst->_a[pst->_top] = x;
+	pst->_top++;
+}
+
+void StackPop(Stack* pst)
+{
+	assert(pst);
+	assert(pst->_top > 0);
+	pst->_top--;
+}
+
+int StackSize(Stack* pst)
+{
+	assert(pst);
+	return pst->_top;
+}
+
+int StackEmpty(Stack* pst)
+{
+	assert(pst);
+	/*if (pst->_top == 0)
+		return 1;
+	else
+		return 0;*/
+	return pst->_top == 0 ? 1 : 0;
+}
+
+STDataType StackTop(Stack* pst)
+{
+	assert(pst);
+	assert(pst->_top > 0);
+	return pst->_a[pst->_top - 1];
+}
+
+typedef struct {
+	Stack _pushST;
+	Stack _popST;
+} MyQueue;
+
+/** Initialize your data structure here. */
+
+MyQueue* myQueueCreate()
+{
+	MyQueue* q = (MyQueue*)malloc(sizeof(MyQueue));
+	StackInit(&q->_pushST);
+	StackInit(&q->_popST);
+	return q;
+}
+
+/** Push element x to the back of queue. */
+void myQueuePush(MyQueue* obj, int x)
+{
+	StackPush(&obj->_pushST, x);
+}
+
+/** Removes the element from in front of queue and returns that element. */
+int myQueuePop(MyQueue* obj)
+{
+	int front = myQueuePeek(obj);
+	StackPop(&obj->_popST);
+	return front;
+}
+
+/** Get the front element. */
+int myQueuePeek(MyQueue* obj)
+{
+	if (!StackEmpty(&obj->_popST))
+	{
+		return StackTop(&obj->_popST);
+	}
+	else
+	{
+		while (!StackEmpty(&obj->_pushST))
+		{
+			StackPush(&obj->_popST, StackTop(&obj->_pushST));
+			StackPop(&obj->_pushST);
+		}
+		return StackTop(&obj->_popST);
+	}
+}
+
+/** Returns whether the queue is empty. */
+bool myQueueEmpty(MyQueue* obj)
+{
+	return StackEmpty(&obj->_pushST) && StackEmpty(&obj->_popST);
+}
+
+void myQueueFree(MyQueue* obj)
+{
+	StackDestory(&obj->_pushST);
+	StackDestory(&obj->_popST);
+	free(obj);
+}
+
+/**
+ * Your MyQueue struct will be instantiated and called as such:
+ * MyQueue* obj = myQueueCreate();
+ * myQueuePush(obj, x);
+
+ * int param_2 = myQueuePop(obj);
+
+ * int param_3 = myQueuePeek(obj);
+
+ * bool param_4 = myQueueEmpty(obj);
+
+ * myQueueFree(obj);
+*/
